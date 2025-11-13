cc0a8a6381b124aa76f64b42bffa9148b5726ab4
Dilan <dilantienda90@gmail.com>
2025-11-12 18:47:14 -0600

    Funcionalidad: Agregando la acción de reiniciar la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import { useReducer, useEffect } from "react"
+import { useReducer, useEffect, useMemo } from "react"
 import Form from "./components/Form"
-import { activityReducer, initialState} from "./reducers/activity-reducer"
+import { activityReducer, initialState } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList";
 
 function App() {
 
   const [state, dispatch] = useReducer(activityReducer, initialState)
 
-  useEffect( ()=>{
+  useEffect(() => {
     localStorage.setItem("activities", JSON.stringify(state.activities));
-  },[state.activities])
+  }, [state.activities])
+
+  const canRestartApp = () => useMemo(() => state.activities.length, [state.activities])
 
   return (
     <>
       <header className="bg-lime-600 py-3">
         <div className="max-w-4xl mx-auto flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">Contador de Calorías</h1>
+
+          <button disabled={!canRestartApp()} onClick={() => dispatch({ type: "restart-app" })} className="disabled:opacity-10 bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm">
+            Reiniciar App
+          </button>
+
         </div>
       </header>
 
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
-          <Form 
+          <Form
             dispatch={dispatch}
             state={state}
           />
@@ -29,7 +36,7 @@ function App() {
       </section>
 
       <section className="p-10 mx-auto max-w-4xl">
-        <ActivityList 
+        <ActivityList
           activities={state.activities}
           dispatch={dispatch}
         />
