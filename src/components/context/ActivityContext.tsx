137b4e39ba52a98eaea41df3754ac29ffaef013b
Dilan <dilantienda90@gmail.com>
2025-12-01 08:58:24 -0600

    Funcion: Moviendo los calculos de calorias al context para tener acceso con el hook

--- src/components/context/ActivityContext.tsx
+++ src/components/context/ActivityContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, type ReactNode } from "react";
+import { createContext, useMemo, useReducer, type ReactNode } from "react";
 import { activityReducer, initialState, type ActivityAction, type ActivityState } from "../../reducers/activity-reducer";
 
 type ActivityProviderProps = {
@@ -8,6 +8,9 @@ type ActivityProviderProps = {
 type ActivityContextProps = {
     state: ActivityState;
     dispatch: React.Dispatch<ActivityAction>;
+    caloriesConsumed: number;
+    caloriesBurned: number;
+    netCalories: number;
 }
 
 export const ActivityContext = createContext<ActivityContextProps>(null!);
@@ -16,8 +19,13 @@ export const ActivityProvicer = ({ children }: ActivityProviderProps) => {
 
     const [state, dispatch] = useReducer(activityReducer, initialState);
 
+    //contadores
+    const caloriesConsumed = useMemo(() => state.activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [state.activities])
+    const caloriesBurned = useMemo(() => state.activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [state.activities])
+    const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [state.activities])
+
     return (
-        <ActivityContext.Provider value={{ state, dispatch }}>
+        <ActivityContext.Provider value={{ state, dispatch, caloriesConsumed, caloriesBurned, netCalories }}>
             {children}
         </ActivityContext.Provider>
     )
